5468d70ae5a8326855c8a6ef765a63d795c20cb9
sisShalOne <mekisis@gmail.com>
2019-11-15 12:15:03 -0500

    adding remote host on the server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-lively-71881',
     user : 'postgres',
     database : 'smart_brain'
 	}
